0e88474bcc067e552c519d1b8aa46aa25a00fc66
Godti Vinod <venkat.godti3@gmail.com>
2019-05-19 22:48:25 +0530

    Node server setup

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 //Importing the express and body-parser
 const express =require('express');
-const config=require('./config');
-
+var path=require('path');
 //Initialize the express app
 const app=express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
   });
+const port = process.env.port || 5000;
 //Server listen on port config.port
-app.listen(config.port,()=>{
-    console.log('Server started and running on the port number '+config.port)
+app.listen(port,()=>{
+    console.log('Server started and running on the port number '+ port)
 });
